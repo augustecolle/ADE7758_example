3a10a7c33f0207957a9e2136332b69577b0b25ec
rienleenders <rienleenders@hotmail.com>
2014-06-12 13:46:32 +0200

    Update na eerste test
    Spanning werkt!

--- ADE7758_example.ino
+++ ADE7758_example.ino
@@ -6,10 +6,12 @@ ADE7758 myADE(10);
 void setup(){
   Serial.begin(9600);
   myADE.begin();
+  
+  myADE.write16(0x,);
 }
 
 void loop(){
   Serial.println(myADE.read8bits(0x11),HEX);
-  Serial.println(myADE.getVRMS(PHASE_A));
+  Serial.println(myADE.getVRMS(PHASE_A)/4912.68);
   delay(1000);
 }
